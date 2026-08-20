9ed62446024c00887b4619459374d74398473c46
ajohnsonOKHS <anistonisabelle@icloud.com>
2026-08-20 13:48:06 +0000

    todo 4

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -62,7 +62,10 @@ $(document).ready(function () {
   }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat)
+    animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
   
@@ -79,7 +82,8 @@ $(document).ready(function () {
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape[i]);
   }
 
   function badDisplay() {
