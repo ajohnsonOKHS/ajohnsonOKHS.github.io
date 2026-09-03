18a57ab7f511df8c000dc07562986be23184f5da
ajohnsonOKHS <anistonisabelle@icloud.com>
2026-09-03 13:58:37 +0000

    fix TODO 8

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -130,9 +130,9 @@ function moveSnake() {
     of snake.head.direction which may be one of "left", "right", "up", or "down"
   */
   if (snake.head.direction === "left") {
-    snake.head.colum = snake.head.colum - 1;
+    snake.head.column = snake.head.column - 1;
   } else if (snake.head.direction === "right") {
-    snake.head.colum = snake.head.colum + 1;
+    snake.head.column = snake.head.column + 1;
   } else if (snake.head.direction === "up") {
     snake.head.row = snake.head.row - 1;
   } else if (snake.head.direction === "down") {
