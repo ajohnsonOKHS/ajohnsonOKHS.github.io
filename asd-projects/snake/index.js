cc3743fcf0c78acc4b774ac5b1d7fb3d7a0ea1ea
ajohnsonOKHS <anistonisabelle@icloud.com>
2026-09-03 14:09:41 +0000

    TODO 9

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -145,6 +145,18 @@ function moveSnake() {
 }
 
 // TODO 9: Create a new helper function
+function moveBodyAToBodyB(bodyA, bodyB) {
+  bodyA.row = bodyB.row;
+  bodyA.column = bodyB.column;
+  bodyA.direction = bodyB.direction;
+}
+
+console.log("Moving body A to body B...");
+setTimeout(() => {
+  moveBodyAToBodyB(snake.body[1], snake.head);
+  repositionSquare(snake.body[1]);
+}, 2_000);
+
 
 function hasHitWall() {
   /* 
