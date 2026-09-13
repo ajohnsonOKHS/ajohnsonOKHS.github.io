a92a17fb71fc569f3a37ef3f415fd59aa80f51fc
ajohnsonOKHS <anistonisabelle@icloud.com>
2026-09-13 22:22:55 +0000

    dice-app corrections

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -8,9 +8,7 @@ $(document).ready(function () {
     .css("top", 50)
     .css("left", 50)
     .appendTo("#die");
-});
 
-$(document).ready(function () {
   $("<div2>")
     .css("height", 15)
     .css("width", 15)
@@ -18,61 +16,61 @@ $(document).ready(function () {
     .css("position", "absolute")
     .css("top", 50)
     .css("left", 50)
-    .appendTo("#secondDie"); 
-});
+    .appendTo("#secondDie");
 
-function makeDot(top, left, dieID) {
-  $("<div>")
-    .css("height", 15)
-    .css("width", 15)
-    .css("background-color", "black")
-    .css("position", "absolute")
-    .css("top", top)
-    .css("left", left)
-    .appendTo(dieID);
-}
-
-function rollDie(dieID) {
-  $(dieID).empty();
-  var randomNum = Math.ceil(Math.random() * 6);
-  //console.log(randomNum);
-  if (randomNum === 1) {
-    makeDot(50, 50, dieID);
-  } else if (randomNum === 2) {
-    makeDot(25, 25, dieID);
-    makeDot(75, 75, dieID);
-  } else if (randomNum === 3) {
-    makeDot(25, 25, dieID);
-    makeDot(75, 75, dieID);
-    makeDot(50, 50, dieID);
-  } else if (randomNum === 4) {
-    makeDot(75, 75, dieID);
-    makeDot(25, 25, dieID);
-    makeDot(25, 75, dieID);
-    makeDot(75, 25, dieID);
-  } else if (randomNum === 5) {
-    makeDot(50, 50, dieID);
-    makeDot(75, 75, dieID);
-    makeDot(25, 25, dieID);
-    makeDot(25, 75, dieID);
-    makeDot(75, 25, dieID);
-  } else if (randomNum === 6) {
-    makeDot(25, 75, dieID);
-    makeDot(50, 75, dieID);
-    makeDot(75, 75, dieID);
-    makeDot(25, 25, dieID);
-    makeDot(50, 25, dieID);
-    makeDot(75, 25, dieID);
+  function makeDot(top, left, elementID) {
+    $("<div>")
+      .css("height", 15)
+      .css("width", 15)
+      .css("background-color", "black")
+      .css("position", "absolute")
+      .css("top", top)
+      .css("left", left)
+      .appendTo(elementID);
   }
-}
 
-function handleClick() {
-  rollDie("#die");
-}
+  function rollDie(elementID) {
+    $(elementID).empty();
+    var randomNum = Math.ceil(Math.random() * 6);
+    //console.log(randomNum);
+    if (randomNum === 1) {
+      makeDot(50, 50, elementID);
+    } else if (randomNum === 2) {
+      makeDot(25, 25, elementID);
+      makeDot(75, 75, elementID);
+    } else if (randomNum === 3) {
+      makeDot(25, 25, elementID);
+      makeDot(75, 75, elementID);
+      makeDot(50, 50, elementID);
+    } else if (randomNum === 4) {
+      makeDot(75, 75, elementID);
+      makeDot(25, 25, elementID);
+      makeDot(25, 75, elementID);
+      makeDot(75, 25, elementID);
+    } else if (randomNum === 5) {
+      makeDot(50, 50, elementID);
+      makeDot(75, 75, elementID);
+      makeDot(25, 25, elementID);
+      makeDot(25, 75, elementID);
+      makeDot(75, 25, elementID);
+    } else if (randomNum === 6) {
+      makeDot(25, 75, elementID);
+      makeDot(50, 75, elementID);
+      makeDot(75, 75, elementID);
+      makeDot(25, 25, elementID);
+      makeDot(50, 25, elementID);
+      makeDot(75, 25, elementID);
+    }
+  }
 
-function handleClick2() {
-  rollDie("#secondDie");
-}
+  function handleClick() {
+    rollDie("#die");
+  }
 
-$("#die").on("click", handleClick);
-$("#secondDie").on("click", handleClick2);
+  function handleClick2() {
+    rollDie("#secondDie");
+  }
+
+  $("#die").on("click", handleClick);
+  $("#secondDie").on("click", handleClick2);
+});
